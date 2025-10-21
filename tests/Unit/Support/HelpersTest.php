df905063cecd9e59fea94e8c8790765b5d8797d9
Andrei Pristavu <andrew@andrew.ro>
2025-10-21 10:29:28 +0300

    test: add unit test for creating a new Anaf instance using helper function

---
+++ tests/Unit/Support/HelpersTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+it('can create a new instance using helper function', function () {
+    $anafInstance = anaf();
+
+    expect($anafInstance)->toBeInstanceOf(Pristavu\Anaf\Anaf::class);
+});
