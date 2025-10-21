d751c2d423b0d2ae737e772612d0628687d3e7dc
Andrei Pristavu <andrew@andrew.ro>
2025-10-21 10:43:11 +0300

    test: specify return type for helper function instance creation test

--- tests/Unit/Support/HelpersTest.php
+++ tests/Unit/Support/HelpersTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-it('can create a new instance using helper function', function () {
+it('can create a new instance using helper function', function (): void {
     $anafInstance = anaf();
 
     expect($anafInstance)->toBeInstanceOf(Pristavu\Anaf\Anaf::class);
